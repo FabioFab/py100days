4cf76300e568549bd2361a34d2c48418b6b1db79
FabSec <fdbfbk@gmail.com>
2020-04-12 23:41:57 +0100

    Creation of new 21-25_Days.py file

--- 21-25_Days.py
+++ 21-25_Days.py
@@ -2,4 +2,20 @@
 # # DAY21
 # ################################
 #
-#
+# import numpy as np
+from numpy import *
+
+# arr01 = {i, [2,3,4,5]}
+arr02 = array([2, 3, 4, 5], int)
+print(arr02)
+
+import numpy as np
+print(np.__version__)
+
+# covert a list into a 1D array (np.array)
+
+listA = [1, 2, 4, 5, 6, 7]
+print("Starting list ", listA)
+
+one_dim_array = np.array(listA, int) # or even 'dtype = int'
+print("My first 1D array is ", one_dim_array)
